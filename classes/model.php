434a0e01753e85542b676f13821d5e75c49380f7
algoritmika <info@algoritmika.com>
2026-03-11 17:47:18 +0200

    Cleanup

--- classes/model.php
+++ classes/model.php
@@ -10,7 +10,7 @@
 defined( 'ABSPATH' ) || exit;
 
 abstract class modelPtw extends baseObjectPtw {
-    protected $_data = array();
+	protected $_data = array();
 	protected $_code = '';
 
 	protected $_orderBy = '';
@@ -24,30 +24,30 @@ abstract class modelPtw extends baseObjectPtw {
 	protected $_lastGetCount = 0;
 	protected $_idField = 'id';
 
-    /*public function init() {
+	/*public function init() {
 
-    }
-    public function get($d = array()) {
+	}
+	public function get($d = array()) {
 
-    }
-    public function put($d = array()) {
+	}
+	public function put($d = array()) {
 
-    }
-    public function post($d = array()) {
+	}
+	public function post($d = array()) {
 
-    }
-    public function delete($d = array()) {
+	}
+	public function delete($d = array()) {
 
-    }
-    public function store($d = array()) {
+	}
+	public function store($d = array()) {
 
-    }*/
+	}*/
 	public function setCode($code) {
-        $this->_code = $code;
-    }
-    public function getCode() {
-        return $this->_code;
-    }
+		$this->_code = $code;
+	}
+	public function getCode() {
+		return $this->_code;
+	}
 	public function getModule() {
 		return framePtw::_()->getModule( $this->_code );
 	}
